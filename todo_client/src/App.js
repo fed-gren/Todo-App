bcb1239c7ffe17d58079a036cb79895708fe6fa6
fed-gren <deveb22@gmail.com>
2019-05-14 20:29:40 +0900

    feat: create a Title component
    - Todo App title로 사용할 Title 컴포넌트 생성

--- todo_client/src/App.js
+++ todo_client/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// components
+import Title from "./components/Title.js";
 
 function App() {
   return (
     <div className="App">
+      <Title />
     </div>
   );
 }
---
+++ todo_client/src/components/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Title() {
+  return(
+    <h1 className="title_todo">Todo</h1>
+  );
+}
+
+export default Title;
