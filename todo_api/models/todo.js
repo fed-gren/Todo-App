2ed4c19d10a1a57da1b9ebe5ad3339e40364db04
fed-gren <deveb22@gmail.com>
2019-05-14 19:59:01 +0900

    feat: create a todo model
    - 설계한 todo table 구조에 맞게 model 작성
    - 등록일, 수정일은 자동으로 생성되므로 따로 만들지 않음

---
+++ todo_api/models/todo.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const Todo = sequelize.define('todo', {
+      id: {
+        type: DataTypes.BIGINT(20),
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      status: {
+        type: DataTypes.ENUM('TODO', 'DONE'),
+        defaultValue: 'TODO'
+      },
+      deadline: {
+        type: DataTypes.DATE
+      },
+      priority: {
+        type: DataTypes.ENUM("1", "2", "3"),
+        defaultValue: "3",
+        allowNull: false
+      }
+    },
+    {
+      freezeTableName: true,
+    }
+  );
+
+  return Todo;
+}
