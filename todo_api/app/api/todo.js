e418779a7fef146879683e6e6fef5ff6719d4124
fed-gren <deveb22@gmail.com>
2019-05-14 19:59:52 +0900

    feat: create a todo api
    - 리소스 생성, 조회, 수정, 삭제 (CRUD) 작성

---
+++ todo_api/app/api/todo.js
@@ -0,0 +1,43 @@
+module.exports = (app, todo) => {
+  //모든 todo 조회
+  app.get("/todos", (req, res) =>
+    todo.findAll().then(result => res.json(result))
+  );
+
+  //특정 todo 조회
+  app.get("/todo/:id", (req, res) => {
+    const { id } = req.params;
+    return todo.findByPk(id).then(result => res.json(result));
+  });
+
+  //todo 생성
+  app.post("/todo", (req, res) => {
+    const { title, content, deadline, priority } = req.body;
+    return todo.create({ title, content, deadline, priority }).then(result => res.json(result));
+  });
+
+  //특정 todo 수정
+  app.put("/todo/:id", (req, res) => {
+    const { title, content, status, deadline, priority } = req.body;
+    const { id } = req.params;
+
+    return todo
+      .update(
+        { title, content, status, deadline, priority },
+        {
+          where: { id }
+        }
+      )
+      .then(result => res.json(result));
+  });
+
+  //특정 todo 삭제
+  app.delete("/todo/:id", (req, res) => {
+    const { id } = req.params;
+    return todo
+      .destroy({
+        where: { id }
+      })
+      .then(result => res.json(result));
+  });
+};
