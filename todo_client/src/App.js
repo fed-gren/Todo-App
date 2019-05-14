3f42a297576efc803476c0f1ff07c009b1bbd100
fed-gren <deveb22@gmail.com>
2019-05-14 20:34:33 +0900

    feat: create a control bar component
    - todo 추가 및 제어 위한 TodoControlBar 컴포넌트 작성

--- todo_client/src/App.js
+++ todo_client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 
 // components
-import Title from "./components/Title.js";
+import Title from "./components/Title";
+import TodoControlBar from "./components/TodoControlBar";
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <TodoControlBar/>
     </div>
   );
 }
---
+++ todo_client/src/components/TodoControlBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoControlBar() {
+  return (
+    <>
+    <span>Alarm</span>
+    <span>Add Todo</span>
+    </>
+  );
+}
+
+export default TodoControlBar;
