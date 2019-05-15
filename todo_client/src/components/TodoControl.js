c9c51276d40cff62ecb917eeb0d81a600e1e588d
fed-gren <deveb22@gmail.com>
2019-05-15 20:27:51 +0900

    feat: create a todo control component
    - markup, css 작성 완료
    - react-icons 사용

--- todo_client/src/components/TodoControl.js
+++ todo_client/src/components/TodoControl.js
@@ -1,10 +1,32 @@
 import React from "react";
+import "../styles/TodoControl.css";
+import { FaBell } from "react-icons/fa";
+import { Button, Form } from "react-bootstrap";
 
 function TodoControl() {
   return (
     <section className="todo_control_container">
-    <span>Alarm</span>
-    <span>Add Todo</span>
+      <section className="todo_control_left">
+        <section className="section_alarm">
+          <FaBell className="alarm" />
+        </section>
+
+        <section className="section_button_newTodo">
+          <Button className="button_newTodo" variant="outline-dark">
+            New
+          </Button>
+        </section>
+      </section>
+
+      <section className="section_guide_message">
+        <span className="guide_message">새로운 할 일을 등록하세요!</span>
+      </section>
+
+      <section className="section_button_delete">
+        <Button className="button_delete" variant="outline-danger">
+          Delete
+        </Button>
+      </section>
     </section>
   );
 }
