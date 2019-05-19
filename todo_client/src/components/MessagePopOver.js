a323646affa08879be9018766d1218d1e9999dce
fed-gren <deveb22@gmail.com>
2019-05-20 03:53:35 +0900

    feat: create interval for check deadline exceed
    - 기한 초과 확인을 위한 setInterval 작성

--- todo_client/src/components/MessagePopOver.js
+++ todo_client/src/components/MessagePopOver.js
@@ -1,37 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Popover, OverlayTrigger } from "react-bootstrap";
 import { FaBell } from "react-icons/fa";
 import "../styles/MessagePopOver.css";
 import axios from "axios";
 
 function MessagePopOver() {
-  const [todos, setTodos] = useState(null);
-  // const [numOfMessage, setNumOfMessage] = useState(0);
+  // const [todos, setTodos] = useState(null);
+  const [numOfMessage, setNumOfMessage] = useState(0);
+  const [renderedTitles, setRenderedTitle] = useState(null);
+
   const getAllTodos = function() {
     axios("http://localhost:8080/todos")
       .then(res => res.data)
-      .then(res => setTodos(res));
-  }
+      .then(todos => {
+        setRenderedTitle(renderTitles(todos));
+      });
+  };
 
-  useEffect(getAllTodos, [(todos && true)]);
-  console.log(todos);
-  console.log(typeof todos);
 
-  let numOfMessage;
-
-  function renderTitles() {
-    if(todos === null) return;
+  function renderTitles(todos) {
+    if (todos === null) return;
+    // console.log(todos);
     const titles = todos.map((todo, index) => {
-      const {title:todoTitle} = todo;
-      return (<div className="out_of_date" key={index}>
-        {todoTitle}
-      </div>)
+      const { title: todoTitle } = todo;
+      return (
+        <div className="out_of_date" key={index}>
+          {todoTitle}
+        </div>
+      );
     });
-    numOfMessage = titles.length;
+    setNumOfMessage(titles.length);
     return titles;
   }
 
-  const renderedTitles = renderTitles();
+  useEffect(() => {
+    getAllTodos();
+    setInterval(() => {
+      getAllTodos();
+    }, 5000);
+  }, []);
 
   const popover = (
     <Popover id="popover-basic" title="일정 지난 Todo">
