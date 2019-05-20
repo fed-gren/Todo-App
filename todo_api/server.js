21cbb606a7c2dec15491cf14ee9f7a920360a3d1
fed-gren <deveb22@gmail.com>
2019-05-20 18:54:32 +0900

    fix: CORS url

--- todo_api/server.js
+++ todo_api/server.js
@@ -5,7 +5,7 @@ const apiTodo = require("./app/api/todo");
 const app = express();
 app.use(express.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
